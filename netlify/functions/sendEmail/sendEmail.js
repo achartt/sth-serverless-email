0cba3cf35b46babad5c635b65c705b68da164694
achartt <80549907+achartt@users.noreply.github.com>
2023-09-05 17:33:21 -0500

    Update sendEmail.js

--- netlify/functions/sendEmail/sendEmail.js
+++ netlify/functions/sendEmail/sendEmail.js
@@ -1,17 +1,43 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+const handler = newFunction()
 
 module.exports = { handler }
+
+function newFunction() {
+    return async (event) => {
+        try {
+            const nodemailer = require('nodemailer');
+
+            exports.handler = async function(event, context) {
+                if (event.httpMethod !== "POST") {
+                    return { statusCode: 405, body: "Method Not Allowed" };
+                }
+
+                const body = JSON.parse(event.body);
+
+                let transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'austin@southerndreamhomes.org',
+                        pass: 'vmmsecxlsazgnors' // You should use an app-specific password here
+                    }
+                });
+
+                let info = await transporter.sendMail({
+                    from: '"Southern Dream Homes" <austin@southerndreamhomes.org>',
+                    to: "austin@southerndreamhomes.org",
+                    subject: "New Form Submission",
+                    text: JSON.stringify(body, null, 2),
+                });
+
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: "Email sent successfully!" }),
+                };
+            };
+
+        } catch (error) {
+            return { statusCode: 500, body: error.toString() };
+        }
+    };
+}
